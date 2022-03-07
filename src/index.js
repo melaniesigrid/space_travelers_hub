f35ad1a738435bd1e5fb462c4e57b3e17edfd46d
Daniel Ufeli <danielufeli@yahoo.com>
2022-03-08 00:18:55 +0100

    Add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
