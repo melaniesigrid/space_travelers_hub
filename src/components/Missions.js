017e4c054491ee678939cdb1689ec64dc0242389
Daniel Ufeli <danielufeli@yahoo.com>
2022-03-08 21:04:19 +0100

    Add mission lists

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,16 +1,44 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getMissions } from '../redux/missions';
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+import MissionList from './MissionList';
 
 const Missions = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getMissions());
-  }, []);
+  const missions = useSelector((state) => state.missionsReducer);
   return (
-    <div>
-      Mission
-    </div>
+    <>
+      <br />
+      <br />
+      <div className="container">
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Mission</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col">.</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                missions.map((mission) => (
+                  <MissionList
+                    id={mission.mission_id}
+                    name={mission.mission_name}
+                    description={mission.description}
+                    key={mission.mission_id}
+                  />
+                ))
+
+              }
+            </tbody>
+          </table>
+        </div>
+
+      </div>
+
+    </>
   );
 };
 
