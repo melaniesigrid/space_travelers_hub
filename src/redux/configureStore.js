eaa872c4263e874a13813e64031757c18a0ccc09
Melanie Arellano <melaniesigrid@protonmail.com>
2022-03-08 10:20:37 -0600

    Create and configure store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { rocketsReducer } from './rockets';
+import { missionsReducer } from './missions';
+
+const reducer = combineReducers({
+  rocketsReducer,
+  missionsReducer,
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger),
+);
+
+export default store;
