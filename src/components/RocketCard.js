7af54d9545a33beb49ca57925a06d7f61619294c
Melanie Arellano <melaniesigrid@protonmail.com>
2022-03-09 10:24:41 -0600

    Create RESERVE_ROCKET action, implement on reducer and update RocketCard to perform rocket reservation on button click

--- src/components/RocketCard.js
+++ src/components/RocketCard.js
@@ -1,5 +1,8 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Card, Button } from 'react-bootstrap';
 import { PropTypes } from 'prop-types';
+import { reserveRocket } from '../redux/rockets';
 
 function RocketCard({
   id,
@@ -7,13 +10,17 @@ function RocketCard({
   description,
   flickrImages,
 }) {
+  const dispatch = useDispatch();
+  const reserveMyRocket = (id) => {
+    dispatch(reserveRocket(id));
+  };
   return (
     <Card key={id} expand="md" className="HorizontalCard">
       <Card.Img variant="left" src={flickrImages} className="RocketImage" />
       <Card.Body>
         <Card.Title>{rocketName}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary" className="ReserveButton">Reserve Rocket</Button>
+        <Button variant="primary" className="ReserveButton" onClick={() => reserveMyRocket(id)}>Reserve Rocket</Button>
       </Card.Body>
     </Card>
   );
